133310012cfe6939841cc19b35003dcded3db48d
calebrudnicki <calebrudnicki@gmail.com>
2017-04-16 19:50:46 -0400

    Added animation to the logo on the login page

--- app/src/main/java/com/example/calebrudnicki/spoutr/LoginActivity.java
+++ app/src/main/java/com/example/calebrudnicki/spoutr/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.example.calebrudnicki.spoutr;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.util.Log;
 import android.view.View;
@@ -22,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button bLogin;
+    private ImageView ivLogo;
     private TextView registerLink;
     private Model modelHelper;
 
@@ -33,8 +37,13 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = (EditText) findViewById(R.id.etUsername);
         etPassword = (EditText) findViewById(R.id.etPassword);
         bLogin = (Button) findViewById(R.id.bLogin);
+        ivLogo = (ImageView) findViewById(R.id.ivLogo);
         registerLink = (TextView) findViewById(R.id.tvRegisterHere);
         modelHelper = Model.getInstance();
+
+        //Animation on the logo
+        android.view.animation.Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.clockwise);
+        ivLogo.startAnimation(animation);
 
         //This function segues the view to the register page upon clicking the register here button
         registerLink.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/calebrudnicki/spoutr/RegisterActivity.java
+++ app/src/main/java/com/example/calebrudnicki/spoutr/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
